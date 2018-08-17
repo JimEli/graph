c66b3ee33b4ab4d27ebb091334c3093e02898137
Jim Eli <31859133+JimEli@users.noreply.github.com>
2018-08-17 14:42:34 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -11,29 +11,30 @@ int main()
 	Graph g;
 	int start = 0, goal = 34;
 
-	// Lets solve this simple maze:
+	// Lets solve this simple 36x36 maze:
 	// S #### - 012345
 	// #  # # - 678901
 	// ## # # - 234567
 	// #    # - 890123
  	// # ## # - 456789
-	// ####D# - 012345
+	// ####G# - 012345
+	// # = wall, S = start and G = end.
 
 	// add_edge(adjacency list, src vertex, dest vertex).
-	g.addEdge(0,1);
-	g.addEdge(1,7);
-	g.addEdge(7,8);
-	g.addEdge(10,16);
-	g.addEdge(14,8);
-	g.addEdge(14,20);
-	g.addEdge(16,22);
-	g.addEdge(19,20);
-	g.addEdge(19,25);
-	g.addEdge(20,13);
-	g.addEdge(20,21);
-	g.addEdge(21,22);
-	g.addEdge(22,28);
-	g.addEdge(28,34);
+	g.addEdge(0, 1);
+	g.addEdge(1, 7);
+	g.addEdge(7, 8);
+	g.addEdge(10, 16);
+	g.addEdge(14, 8);
+	g.addEdge(14, 20);
+	g.addEdge(16, 22);
+	g.addEdge(19, 20);
+	g.addEdge(19, 25);
+	g.addEdge(20, 13);
+	g.addEdge(20, 21);
+	g.addEdge(21, 22);
+	g.addEdge(22, 28);
+	g.addEdge(28, 34);
 
 	g.printShortestDistance(start, goal);
 }
